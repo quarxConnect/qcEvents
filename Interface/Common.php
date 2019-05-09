2a00e19b19684fac87c374946fd83f3c713cc89e
Bernd Holzmüller <bernd@quarxconnect.de>
2019-05-09 23:35:01 +0200

    Added getEventBase()-Call to Interface_Common

--- Interface/Common.php
+++ Interface/Common.php
@@ -21,6 +21,18 @@
   require_once ('qcEvents/Interface/Hookable.php');
   
   interface qcEvents_Interface_Common extends qcEvents_Interface_Hookable {
+    // {{{ getEventBase
+    /**
+     * Retrive the handle of the current event-loop-handler
+     * 
+     * @access public
+     * @return qcEvents_Base May be NULL if none is assigned
+     * 
+     * @remark This is implemented by qcEvents_Trait_Parented
+     **/
+    public function getEventBase ();
+    // }}}
+    
     // {{{ setEventBase
     /**
      * Set the Event-Base of this source
